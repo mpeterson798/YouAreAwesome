5555e318d0c93a1ccb059f0d973774252fc1e7d0
Matthew Peterson <matthew251125@icloud.com>
2025-09-05 17:02:46 -0500

    After variable button lesson.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a programmer!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.red)
+            Button("Button") {
+                message = "You pressed the button!"
+            }
         }
         .padding()
     }
